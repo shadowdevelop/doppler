683ab5d81e22d273bff298d97876ac4ff17dd1d1
Roacho Develop <angel.roacho@gmail.com>
2023-01-30 12:18:38 -0600

    Update alertafunciones.py

--- alertafunciones.py
+++ alertafunciones.py
@@ -4,22 +4,25 @@ import serial
 import logging
 
 def enviarmensaje(mensaje):
-    serial_mensaje = serial.Serial() 
-    serial_mensaje = serial.Serial(
-        baudrate=9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1,
-        writeTimeout=2
-    )
+    try:
+        serial_mensaje = serial.Serial() 
+        serial_mensaje = serial.Serial(
+            baudrate=9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1,
+            writeTimeout=2
+        )
 
-    serial_mensaje.port = "/dev/ttyUSB0"  # good for linux
-    serial_mensaje.open()
-    serial_mensaje.flushInput()
-    serial_mensaje.flushOutput()
+        serial_mensaje.port = "/dev/ttyUSB0"  # good for linux
+        serial_mensaje.open()
+        serial_mensaje.flushInput()
+        serial_mensaje.flushOutput()
 
-    serial_mensaje.write(str.encode(mensaje))
+        serial_mensaje.write(str.encode(mensaje))
+    except:
+        print("error monitor")
 
 
 #enviarmensaje("","")
